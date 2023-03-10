733704347061cd359c0097024cc1577061f23dcc
Ignacio Campos <ignacio.cvzla@gmail.com>
2023-03-10 12:39:50 -0700

    10 de Marzo 2
    Leccion 127

--- src/main/java/com/example/empleos/controller/HomeController.java
+++ src/main/java/com/example/empleos/controller/HomeController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -110,7 +111,9 @@ public class HomeController {
 	public String buscar(@ModelAttribute("search") Vacante vacante, Model model) {
 		System.out.println("Buscando: " + vacante);
 		
-		Example<Vacante> example = Example.of(vacante);
+		ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("descripcion", ExampleMatcher.GenericPropertyMatchers.contains());
+		
+		Example<Vacante> example = Example.of(vacante, matcher);
 		List<Vacante> lista = serviceVacantes.buscarByExample(example);
 		model.addAttribute("vacantes", lista);
 		
